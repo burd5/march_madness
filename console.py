171411c7ab327761750e43b6686d598dbec85a13
burd5 <austinburdette5@gmail.com>
2024-03-18 11:51:56 -0600

    connection string

--- console.py
+++ console.py
@@ -1,9 +1,9 @@
 import psycopg2
-import streamlit as st
 from st_supabase_connection import SupabaseConnection
 import pandas as pd
 from sqlalchemy import create_engine
 from sqlalchemy import text
-from settings import SQL_ACLH_STRING
+from frontend.settings import USER, DATABASE
+from migrations.csv_to_pg import read_csvs_to_pg, data_file_path
 
 
--- migrations/csv_to_pg.py
+++ migrations/csv_to_pg.py
@@ -2,14 +2,14 @@ import pandas as pd
 from sqlalchemy import create_engine
 import psycopg2
 import os
-from ..streamlit.settings import USER,DATABASE
+from frontend.settings import USER,DATABASE
 
 data_file_path = os.listdir('/Users/austinburdette/Documents/Projects/march_madness/data/')
 
 def read_csvs_to_pg(files):
     engine = create_engine(f'postgresql://{USER}@localhost:5432/{DATABASE}')
     for file in files:
-        df = pd.read_csv(f'/Users/austinburdette/Documents/Projects/march_madness/data/{file}', sep=',')
+        df = pd.read_csv(f'/Users/austinburdette/Documents/Projects/march_madness/data/{file}', sep=',', encoding='latin-1')
         filename = create_file_name(file)
         df.to_sql(filename, engine, if_exists="replace")
 
